10f81af732ae4653175908eacd774e30df5a91ea
Saeed <85992278+SaeedAnsarifard@users.noreply.github.com>
2024-08-09 01:19:58 +0330

    tutorial is updated.

--- framework.py
+++ framework.py
@@ -103,8 +103,8 @@ class FrameWork(object):
         self.det_su = 365.25 * 24. * 6. * 6. * (10/18) * 1/m_p #number of target in kilo ton in a year times 10^{35}
         #B8 phi SNO : 5.25e \times 10^6 cm^2 s^-1
         borom_spec,borom_total = BoromUnoscilated(self.t_e['B8'][0],self.e_nu['B8'][0],self.spec['B8'][0],g,m_e,self.uppt,self.data_su,self.res)
-        self.borom_unoscilated = self.det_su * (2 * np.pi/self.year) * 5.25e-4 * (self.a**2/self.h) * borom_spec
-        self.borom_unoscilated_total = self.det_su * (2 * np.pi/self.year) * 5.25e-4 * (self.a**2/self.h) * borom_total
+        self.borom_unoscilated_spectrum = self.det_su * (2 * np.pi/self.year) * 5.25e-4 * (self.a**2/self.h) * borom_spec
+        self.borom_unoscilated_total    = self.det_su * (2 * np.pi/self.year) * 5.25e-4 * (self.a**2/self.h) * borom_total
         
         self.dr_dldt    = [{'pp' :[[]] , 'Be7' :[[],[]] , 'pep' :[[]] , 'B8' :[[]] } for i in range(self.m12.shape[0])]
         self.components = ['pp','Be7','pep','B8']
@@ -293,7 +293,7 @@ def AveragedPerdiction(dr_dldt,frame):
     year       = frame.year
     theta      = frame.theta
     det_su     = frame.det_su
-    b8_un      = frame.borom_unoscilated
+    b8_un      = frame.borom_unoscilated_spectrum
     res        = frame.res
     components = frame.components
     len_m12    = len(dr_dldt)
